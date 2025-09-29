3d1dc7d37339edd298d51327e8362cdf5bc1a21b
pankaj3008 <pancholipankaj255@gmail.com>
2025-09-29 12:29:45 +0530

    second commit

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,22 +1,10 @@
-// import {Demo} from './components/demo.jsx'
-// import {Parent} from './components/parent.jsx'
-// import {Btn} from './components/Btn.jsx'
-// import {A} from './components/A.jsx' 
-// import { Shop } from './components/shoping.jsx'
-// import { Task } from './components/task.jsx'
-// import { Counter } from './counter.jsx'
+
 import { CalculateInterest } from './components/intrest.jsx'
 
 export function App() {
 
   return (
     <>    
-    {/* <Parent/> */}
-    {/* <Btn/> */}
-    {/* <A/> */}
-    {/* <Shop/> */}
-    {/* <Task/> */}
-    {/* <Counter/> */}
     <CalculateInterest/>  
     </>
   )
